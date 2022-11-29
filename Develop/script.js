9fb77f5648fb5f7c43bc4f1974b7bf1e44e80e70
unknown <peckham4563@gmail.com>
2022-11-29 14:23:31 -0600

    Files modified

--- Develop/script.js
+++ Develop/script.js
@@ -14,10 +14,50 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  $(".time-div").each(function () {
+
+    var timeDiv = $(this).attr("id").val();
+
+      if (currentHour == timeDiv) {
+        $(this).removeClass("future")
+        $(this).removeClass("past")
+        $(this).addClass("present")
+  
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present")
+        $(this).removeClass("past")
+        $(this).addClass("future")
+  
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+        $(this).addClass("past")
+      }
+    })
+    $(".time-div-data-set").each(function () {
+      let timeDiv = $(this).data('hour')
+      if (currentHour == timeDiv) {
+        $(this).removeClass("future")
+        $(this).removeClass("past")
+        $(this).addClass("present")
+  
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present")
+        $(this).removeClass("past")
+        $(this).addClass("future")
+  
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+        $(this).addClass("past")
+      }
+    })
+  })
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+;
