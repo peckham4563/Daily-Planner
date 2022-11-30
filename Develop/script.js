62e82c4ec662c489e38b96abd470e0005d176751
unknown <peckham4563@gmail.com>
2022-11-29 22:05:34 -0600

    Files

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready (function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,16 +9,24 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  $('.saveBtn').on('click', function() {
+    var input = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+
+    localStorage.setItem(input,time);
+  })
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  using dayjs var currentHour = ?
 
   $(".time-block").each(function () {
-
+    var currentHour = dayjs().format("H")
     var timeDiv = $(this).attr("id")
 
       if (currentHour == timeDiv) {
@@ -37,7 +45,6 @@ $(function () {
         $(this).addClass("row time-block past")
       }
     })
-    //.container-fluid px-5 is probably an incorrect element
     $(".conatiner-fluid px- 5").each(function () {
       let timeDiv = $(this).data('hour')
       if (currentHour == timeDiv) {
@@ -62,5 +69,8 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+  $('#9 .description').val(localStorage.getItem('9'));
+
   // TODO: Add code to display the current date in the header of the page.
 ;
