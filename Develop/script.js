b6af71d4bbb1302819d3fbfb41ac4dbf328ecd5a
unknown <peckham4563@gmail.com>
2022-11-29 22:46:49 -0600

    completed files

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,7 @@ $(document).ready (function () {
     var time = $(this).parent().attr('id');
 
 
-    localStorage.setItem(input,time);
+    localStorage.setItem(time,input);
   })
 
 
@@ -71,14 +71,14 @@ $(document).ready (function () {
   //
 
   $('#9 .description').val(localStorage.getItem('9'));
-  $('#9 .description').val(localStorage.getItem('10'));
-  $('#9 .description').val(localStorage.getItem('11'));
-  $('#9 .description').val(localStorage.getItem('12'));
-  $('#9 .description').val(localStorage.getItem('13'));
-  $('#9 .description').val(localStorage.getItem('14'));
-  $('#9 .description').val(localStorage.getItem('15'));
-  $('#9 .description').val(localStorage.getItem('16'));
-  $('#9 .description').val(localStorage.getItem('17'));
+  $('#10 .description').val(localStorage.getItem('10'));
+  $('#11 .description').val(localStorage.getItem('11'));
+  $('#12 .description').val(localStorage.getItem('12'));
+  $('#13 .description').val(localStorage.getItem('13'));
+  $('#14 .description').val(localStorage.getItem('14'));
+  $('#15 .description').val(localStorage.getItem('15'));
+  $('#16 .description').val(localStorage.getItem('16'));
+  $('#17 .description').val(localStorage.getItem('17'));
 
 
 
